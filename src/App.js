130cd9c4630bbd7bae6bfeb4815a3052cedf266b
Nhi Khong Khong <wutianron@gmail.com>
2020-07-15 18:53:59 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import PostGrid from "./components/PostGrid"
 import './App.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(false);
+  const [visible, setVisible] = useState(true);
+  const [userName, setUserName] = useState();
+  const [userID, setUserID] = useState();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const userResult = await axios(`https://jsonplaceholder.typicode.com/users`)
+      console.log(userResult.data)
+      setUsers(userResult.data)
+    }
+    fetchUsers()
+  }, [])
+
+  const handleSubmit = e => {
+    setError(false);
+    const res = users.filter(user => user.username.localeCompare(userName) == 0);
+    console.log("result",res);
+    if(res.length == 0){
+      setError(true);
+    }
+    else{
+      console.log(res)
+      setUserID(res[0].id);
+      setVisible(false);
+    
+  };
+  }
+  const handleChange = e => {
+    setUserName(e.target.value)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="login-form">
+          <label>
+            Username:
+          <input type="text" onChange={handleChange} />
+          </label>
+          <input type="button" onClick={handleSubmit} value="Login" />
+      </div>
+      {error ? <h1 style={{color: "red"}}>Cannot find the user</h1>:<></>}
+      <div className="character-grid">
+        <PostGrid visible={visible} userID={userID} />
+      </div>
     </div>
   );
-}
+  
+  }
 
 export default App;
